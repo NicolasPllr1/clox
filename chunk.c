a0e473595e6364f086595988b7f67820eb1fc411
NicolasPllr1 <nicolas.pllr1@gmail.com>
2025-12-06 12:34:40 +0100

    Init clox with op_return and op_constant

---
+++ chunk.c
@@ -0,0 +1,36 @@
+#include "chunk.h"
+#include "memory.h"
+#include "value.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void initChunk(Chunk *chunk) {
+  chunk->capacity = 0;
+  chunk->count = 0;
+  chunk->code = NULL;
+  initValueArray(&chunk->constants);
+}
+
+void freeChunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
+  initChunk(chunk);
+}
+
+void writeChunk(Chunk *chunk, uint8_t byte) {
+  if (chunk->capacity <= chunk->count) {
+    int old_capacity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(old_capacity);
+    chunk->code =
+        GROW_ARRAY(uint8_t, chunk->code, old_capacity, chunk->capacity);
+  }
+
+  chunk->code[chunk->count] = byte;
+  chunk->count++;
+}
+
+int addConstant(Chunk *chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
+}
---
+++ debug.c
@@ -0,0 +1,39 @@
+#include "debug.h"
+#include "chunk.h"
+#include <stdio.h>
+
+void disAssembleChunk(Chunk *chunk, const char *name) {
+  printf("== %s ==\n", name);
+
+  for (int offset = 0; offset < chunk->count;) {
+    offset = disAssembleInstruction(chunk, offset);
+  }
+}
+
+int simpleInstruction(const char *name, int offset) {
+  printf("%s\n", name);
+  return offset + 1;
+}
+
+int constantInstruction(const char *name, Chunk *chunk, int offset) {
+  int constant = chunk->code[offset + 1];
+  printf("%-16s %4d '", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("\n");
+  return offset + 2;
+}
+
+int disAssembleInstruction(Chunk *chunk, int offset) {
+  printf("%04d ", offset);
+
+  uint8_t instruction = chunk->code[offset];
+  switch (instruction) {
+  case OP_CONSTANT:
+    return constantInstruction("OP_CONSTANT", chunk, offset);
+  case OP_RETURN:
+    return simpleInstruction("OP_RETURN", offset);
+  default:
+    printf("Unknown opcode: %d\n", instruction);
+    return offset + 1;
+  }
+}
