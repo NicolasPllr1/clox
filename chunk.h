c92ca2a7930085b9927b82b132db681ae11d1782
NicolasPllr1 <nicolas.pllr1@gmail.com>
2025-12-06 12:44:51 +0100

    Add line numbers

--- chunk.h
+++ chunk.h
@@ -15,11 +15,12 @@ typedef struct {
 
   uint8_t *code;
   ValueArray constants;
+  int *lines;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 int addConstant(Chunk *chunk, Value value);
 
 #endif
