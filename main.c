1bf65adcabe675e5fe1cbdba2c3cdece171dd1a5
NicolasPllr1 <nicolas.pllr1@gmail.com>
2025-12-07 13:09:54 +0100

    Add binary arithmetic operators
    Addition, substraction, multiplication, division with an example in main

--- main.c
+++ main.c
@@ -6,12 +6,30 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
+  // -((1.2 + 3.4) / 5.6)
+
+  // '(1.2 + 3.4)'
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 1);
   writeChunk(&chunk, constant, 1);
 
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 1);
+  writeChunk(&chunk, constant, 1);
+
+  writeChunk(&chunk, OP_ADD, 1);
+
+  // '/ 5.6'
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 1);
+  writeChunk(&chunk, constant, 1);
+
+  writeChunk(&chunk, OP_DIVIDE, 1);
+
+  // '-'
   writeChunk(&chunk, OP_NEGATE, 1);
 
+  // return
   writeChunk(&chunk, OP_RETURN, 2);
 
   interpret(&chunk);
--- vm.c
+++ vm.c
@@ -26,6 +26,12 @@ void freeVM(){};
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)                                                          \
+  do {                                                                         \
+    double b = pop();                                                          \
+    double a = pop();                                                          \
+    push(a op b);                                                              \
+  } while (false)
 
   for (;;) {
 
@@ -46,6 +52,18 @@ static InterpretResult run() {
       printValue(pop());
       printf("\n");
       return INTERPRET_OK;
+    case OP_ADD:
+      BINARY_OP(+);
+      break;
+    case OP_SUBSTRACT:
+      BINARY_OP(-);
+      break;
+    case OP_MULTIPLY:
+      BINARY_OP(*);
+      break;
+    case OP_DIVIDE:
+      BINARY_OP(/);
+      break;
     case OP_NEGATE:
       push(-pop());
       break;
@@ -58,6 +76,7 @@ static InterpretResult run() {
   }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 };
 
 InterpretResult interpret(Chunk *chunk) {
