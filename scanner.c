c82ea977b090c14da0524b29e16cc96857a1cc96
NicolasPllr1 <nicolas.pllr1@gmail.com>
2025-12-08 21:59:11 +0100

    fix: correct token length calculation

--- scanner.c
+++ scanner.c
@@ -32,7 +32,7 @@ static Token makeToken(TokenType type) {
   Token token;
   token.type = type;
   token.start = scanner.start;
-  token.length = (int)(scanner.start - scanner.current);
+  token.length = (int)(scanner.current - scanner.start);
   token.line = scanner.line;
   return token;
 }
