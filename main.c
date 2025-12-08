8652c28c4411dca10b7042d29727b45066c3fc21
NicolasPllr1 <nicolas.pllr1@gmail.com>
2025-12-08 11:32:23 +0100

    Update main: drop into REPL or process a source file

--- main.c
+++ main.c
@@ -1,39 +1,65 @@
-#include "chunk.h"
 #include "vm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int repl() {
+  // Run, Eval, Print, Loop
+  char line[1024];
+  for (;;) {
+    printf("> ");
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
+    interpret(line);
+  }
+}
+
+static char *readFile(const char *path) {
+  FILE *file = fopen(path, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file \"%s\".\n", path);
+    exit(74);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file);
+  rewind(file);
+
+  char *buffer = (char *)malloc(fileSize + 1);
+  if (buffer == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+  }
+
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  if (bytesRead < fileSize) {
+    fprintf(stderr, "Could not read file \"%s\".\n", path);
+  }
+
+  buffer[bytesRead] = '\0';
+
+  fclose(file);
+  return buffer;
+}
+
+int runFile(const char *path) {
+  char *source = readFile(path);
+  InterpretResult result = interpret(source);
+  if (result == INTERPRET_COMPILE_ERROR)
+    exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR)
+    exit(70);
+}
 
 int main(int argc, const char *argv[]) {
-  initVM();
-  Chunk chunk;
-  initChunk(&chunk);
-
-  // -((1.2 + 3.4) / 5.6)
-
-  // '(1.2 + 3.4)'
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 1);
-  writeChunk(&chunk, constant, 1);
-
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 1);
-  writeChunk(&chunk, constant, 1);
-
-  writeChunk(&chunk, OP_ADD, 1);
-
-  // '/ 5.6'
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 1);
-  writeChunk(&chunk, constant, 1);
-
-  writeChunk(&chunk, OP_DIVIDE, 1);
-
-  // '-'
-  writeChunk(&chunk, OP_NEGATE, 1);
-
-  // return
-  writeChunk(&chunk, OP_RETURN, 2);
-
-  interpret(&chunk);
-  freeVM();
-  freeChunk(&chunk);
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
   return 0;
 }
